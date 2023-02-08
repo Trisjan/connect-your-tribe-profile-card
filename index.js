1619f728b72075370e4512a29620cec82cf38a9e
Trisjan <TrisjanM@hotmail.com>
2023-02-08 14:20:30 +0100

    APpi connection fixed and data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express, { json, response } from 'express'
 
 // Maak een nieuwe express app aan
 const app = express()
+const url = ('https://whois.fdnd.nl/api/v1/member/trisjan-mustamu')
+const data = await fetch(url).then(response => response.json())
 
 // Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
@@ -14,7 +16,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
--- public/scripts/script.js
+++
@@ -1,3 +0,0 @@
-fetch('https://whois.fdnd.nl/api/v1/members')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
